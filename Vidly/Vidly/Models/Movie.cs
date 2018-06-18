44d49cc574c935b2e01f1e6325b16b5e5b9658b6
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-06-18 14:46:25 -0400

    End of Section 1

---
+++ Vidly/Vidly/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
